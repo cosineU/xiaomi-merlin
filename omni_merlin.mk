31a324862154165a1ed31000e67d6c784ecd6b0d
hikuotk <64579850+cosineU@users.noreply.github.com>
2022-12-21 13:23:01 +0800

    omni -> twrp

--- omni_merlin.mk
+++ omni_merlin.mk
@@ -9,14 +9,14 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+# Inherit some common twrp stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from merlin device
 $(call inherit-product, device/xiaomi/merlin/device.mk)
 
 PRODUCT_DEVICE := merlin
-PRODUCT_NAME := omni_merlin
+PRODUCT_NAME := twrp_merlin
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := merlin
 PRODUCT_MANUFACTURER := xiaomi
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_merlin-user
-add_lunch_combo omni_merlin-userdebug
-add_lunch_combo omni_merlin-eng
+add_lunch_combo twrp_merlin-user
+add_lunch_combo twrp_merlin-userdebug
+add_lunch_combo twrp_merlin-eng
